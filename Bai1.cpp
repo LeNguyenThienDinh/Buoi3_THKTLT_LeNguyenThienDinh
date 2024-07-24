9bb464c218708c69332e2fe52460dbcfa657caa6
ThienDinh <hellow12343@gmail.com>
2024-07-24 13:50:26 +0700

    Tao xong ham main voi switch case de thuc hien in ma tran

--- Bai1.cpp
+++ Bai1.cpp
@@ -18,5 +18,43 @@ void taoMaTran(int **mt, int m, int n, int thap, int cao)
 //============================xu_li_main
 int main()
 {
-
+	int m, n, thap, cao, chon;
+	int **mt = NULL;
+	printf("Nhap vao so dong: \n");
+	scanf("%d", &m);
+	printf("nhap vao so cot: \n");
+	scanf("%d", &n);
+	printf("Nhap vao gioi han duoi ngau nhien: \n");
+	scanf("%d", &thap);
+	printf("Nhap vao gioi han tren ngau nhien: \n");
+	scanf("%d", &cao);
+	if (m <= 0 || n <= 0)
+	{
+		printf("Dong va cot phai lon hon 0!!!\n");
+		return 1;
+	}
+	if (thap > cao)
+	{
+		printf("Gioi han duoi phai nho hon hoac bang gioi han tren \n");
+		return 1;
+	}
+	mt = (int **)malloc(m * sizeof(int *));
+	for (int i = 0; i < m; ++i)
+	{
+		mt[i] = (int *)malloc(n * sizeof(int));
+	}
+	taoMaTran(mt, m, n, thap, cao);
+	while (1)
+	{
+		printf("1.tao ma tran ngau nhien \n");
+		printf("Hay nhap lua chon cua ban: \n");
+		scanf("%d", &chon);
+		switch (chon)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		}
+	}
 }
