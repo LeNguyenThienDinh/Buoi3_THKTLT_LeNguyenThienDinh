5e58ed213df795220d49c2913a122cc30f3e7982
ThienDinh <hellow12343@gmail.com>
2024-07-24 16:36:19 +0700

    hoan thanh chuc nang tao ma tran vuong cap n>=5

--- Bai2.cpp
+++ Bai2.cpp
@@ -21,7 +21,7 @@ void inMaTranVuong(int **maTran, int n)
 	{
 		for (int j = 0; j < n; ++j)
 		{
-			printf("%d", maTran[i][j]);
+			printf("%d\t", maTran[i][j]);
 
 		}
 		printf("\n");
@@ -29,3 +29,65 @@ void inMaTranVuong(int **maTran, int n)
 }
 
 //== == == == == == == == == == == == == xu_li_main == == == == == == == == == == == == == == == ==
+int main() 
+{
+	int n, thap, cao, luaChon;
+	int **maTran;
+
+	do 
+	{
+		printf("nhap n cho ma tran (n >= 5): ");
+		scanf("%d", &n);
+		if (n < 5) {
+			printf("n cua ma tran phai lon hon hoac bang 5.\n");
+		}
+	} while (n < 5);
+
+	printf("nhap gioi han duoi: ");
+	scanf("%d", &thap);
+	printf("nhap gioi han tren: ");
+	scanf("%d", &cao);
+
+	if (thap > cao) 
+	{
+		printf("gioi han duoi phai nho hon gioi han tren.\n");
+		return 1;
+	}
+	maTran = (int **)malloc(n * sizeof(int *));
+	for (int i = 0; i < n; ++i)
+	{
+		maTran[i] = (int *)malloc(n * sizeof(int));
+	}
+
+	while (1) 
+	{
+		printf("\nMenu:\n");
+		printf("1. tao va hien thi ma tran vuong\n");
+		printf("7. thoat\n");
+		printf("nhap lua chon: ");
+		scanf("%d", &luaChon);
+
+		switch (luaChon)
+		{
+		case 1:
+			taoMaTranVuong(maTran, n, thap, cao);
+			printf("ma tran vuong co cap %d:\n", n);
+			inMaTranVuong(maTran, n);
+			break;
+		case 2:
+			
+			break;
+		case 7:
+			for (int i = 0; i < n; ++i) {
+				free(maTran[i]);
+			}
+			free(maTran);
+			printf("thoat chuong trinh.\n");
+			return 0;
+		default:
+			printf("lua chon khong hop le hay nhap lai\n");
+		}
+	}
+
+	return 0;
+}
